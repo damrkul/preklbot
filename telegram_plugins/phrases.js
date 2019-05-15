91b4dc6d3d144078e6329986b42c1867a459fd6c
jrankin35 <jrankin35@gatech.edu>
2019-05-15 14:09:34 -0400

    modified phrases to for multi bot use

---
+++ telegram_plugins/phrases.js
@@ -0,0 +1,34 @@
+
+var getCollection = function(collection, msg) {
+    absURL = Meteor.absoluteUrl();
+    // Get Port
+    port = absURL.split(":")[2].split("/")[0];
+    port = parseInt(port)+1;
+
+    // Get MongoURL
+    mongoURL = Meteor.absoluteUrl().split("//")[1].split(":")[0];
+
+    var url = "mongodb://"+ mongoURL + ":"+ String(port) + "/meteor";
+
+
+    if (typeof(msg.text) === "undefined") { return; }
+    global.MongoClient.connect(url,{ useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("meteor");
+        dbo.collection(collection).find({}).toArray((err, result) => {
+            result.forEach(function(element) {
+                msg_txt = msg.text.toString().toLowerCase();
+                element_txt = element.phrase.toString().toLowerCase();
+                if (msg_txt.includes(element_txt)) {
+                    bot.sendMessage(msg.chat.id, element.reply );
+                }
+            });
+            db.close();
+        });
+    });
+}
+
+
+bot.on('message', (msg) => {
+  getCollection("phrases",msg);
+})
