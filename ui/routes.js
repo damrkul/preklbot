02369025cd54af0b01a2607388c790fd087cb3af
jrankin35 <jrankin35@gatech.edu>
2019-08-23 14:18:08 -0400

    see if this keeps changes

--- ui/routes.js
+++ ui/routes.js
@@ -1,9 +1,18 @@
 
+
+// override with mini-pages navigate method
+Meteor.navigateTo = function (path) {
+  FlowRouter.go(path)
+}
+
+import "/ui/layout/example.html"
+import "/ui/layout/example.css"
+
 import "/ui/main/main.html"
 FlowRouter.route('/', {
   name: 'main.show',
   action() {
-  BlazeLayout.render('main', {});
+  BlazeLayout.render('mainLayout', {content:'main'});
   }
 });
 
@@ -12,7 +21,7 @@ import "/ui/match_status/match_status.html"
 FlowRouter.route('/match_status/', {
   name: 'match_status.show',
   action() {
-  BlazeLayout.render('match_status', {});
+  BlazeLayout.render('mainLayout', {content: 'match_status'});
   }
 });
 
@@ -20,7 +29,7 @@ import "/ui/tagging/tagging_main.html"
 FlowRouter.route('/tagging', {
   name: 'tagging_main.show',
   action() {
-  BlazeLayout.render('tagging_main', {});
+  BlazeLayout.render('mainLayout', {content: 'tagging_main'});
   }
 });
 
@@ -29,7 +38,7 @@ import "/ui/r1_match_status/match_status.html"
 FlowRouter.route('/r1_match_status', {
   name: 'r1_match_status.show',
   action() {
-  BlazeLayout.render('r1_match_status', {});
+  BlazeLayout.render('mainLayout', {content: 'r1_match_status'});
   }
 });
 
@@ -39,6 +48,94 @@ import "/ui/lawn_match_status/match_status.html"
 FlowRouter.route('/lawn_match_status', {
   name: 'lawn_match_status.show',
   action() {
-  BlazeLayout.render('lawn_match_status', {});
+  BlazeLayout.render('mainLayout', {content: 'lawn_match_status'});
   }
 });
+
+
+
+FlowRouter.notFound = {
+  action: function () {
+    BlazeLayout.render("noHeaderLayout", {content: "not_found"})
+  }
+}
+
+import "/ui/layout/layout.html"
+FlowRouter.route('/start', {
+  action: function () {
+    BlazeLayout.render('mainLayout', {content: 'main'})
+  }
+})
+
+
+// DEF STATS
+
+import "/ui/def_stats/main/main.html"
+FlowRouter.route('/def_stats', {
+  action: function () {
+    BlazeLayout.render('mainLayout', {content: 'def_stats_main'})
+  }
+})
+
+import "/ui/def_stats/oplays/oplays.html"
+FlowRouter.route('/add_offplays', {
+  action: function () {
+    BlazeLayout.render('mainLayout', {content: 'o_play'})
+  }
+})
+
+
+import "/ui/def_stats/dplays/dplays.html"
+FlowRouter.route('/add_defplays', {
+  action: function () {
+    BlazeLayout.render('mainLayout', {content: 'd_play'})
+  }
+})
+
+
+import "/ui/def_stats/find_lc/find_lc.html"
+FlowRouter.route('/find_lc', {
+  action: function () {
+    BlazeLayout.render('mainLayout', {content: 'find_lc'})
+  }
+})
+
+/*
+import "/ui/def_stats/find_lc/find_lc.html"
+FlowRouter.route('/find_lc_bot', {
+  action: function (params, queryParams ) {
+    var color = FlowRouter.getQueryParam("play");
+      BlazeLayout.render('find_lc', {content: 'find_lc'})
+  }
+})
+*/
+
+
+import "/ui/pww_scoresheet/pww_scoresheet.html"
+FlowRouter.route('/pww_scoresheet', {
+  action: function () {
+    BlazeLayout.render('mainLayout', {content: 'pww_scoresheet'})
+  }
+})
+
+
+import "/ui/admin/admin.html"
+FlowRouter.route('/admin', {
+  action: function () {
+    BlazeLayout.render('mainLayout', {content: 'admin'})
+  }
+})
+
+
+import "/ui/admin/add_user.html"
+FlowRouter.route('/admin/add_user', {
+  action: function () {
+    BlazeLayout.render('mainLayout', {content: 'add_user'})
+  }
+})
+
+
+
+FlowRouter.route('/signout', {
+    action: App.signout
+})
